fef9efc940043d8f1cd7ad958ab1432522745c5d
Espen Høgbakk <espen@hogbakk.no>
2014-06-22 09:24:19 +0200

    Remove unnecessary bundler.

--- roles/rails.rb
+++ roles/rails.rb
@@ -1,12 +1,12 @@
 name 'rails'
 description 'This role configures a Rails stack using Unicorn'
-run_list "role[base]", "recipe[packages]", "recipe[bundler]", "recipe[bluepill]", "recipe[nginx]", "recipe[rails]", "recipe[ruby_build]", "recipe[rbenv]", "recipe[git]", "recipe[ssh_deploy_keys]", "recipe[postfix]"
+run_list "role[base]", "recipe[packages]", "recipe[bluepill]", "recipe[nginx]", "recipe[rails]", "recipe[ruby_build]", "recipe[rbenv]", "recipe[git]", "recipe[ssh_deploy_keys]"
 default_attributes(
   "nginx" => { "server_tokens" => "off" },
   "rbenv" => {
     "group_users" => ['deploy']
   },
   "deploy_users" => [
-        "deploy"
+    "deploy"
   ]
 )
