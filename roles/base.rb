eba5ea4f96b30d8d1a91e9548b40b2cec7654617
Espen Høgbakk <espen@hogbakk.no>
2014-07-08 10:22:08 +0200

    Remove sudo.

--- roles/base.rb
+++ roles/base.rb
@@ -1,15 +1,3 @@
 name 'base'
 description 'Base bootstrap for every box'
-run_list "recipe[sudo]", "recipe[yum]", "recipe[build-essential]", "recipe[ruby]"
-default_attributes(
-  "authorization" => {
-    "sudo" => {
-      "passwordless" => true,
-      "include_sudoers_d" => true,
-      "sudoers_default" => [
-        'env_reset',
-        'secure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"'
-      ],
-    }
-  }
-)
+run_list "recipe[yum]", "recipe[build-essential]", "recipe[ruby]"
